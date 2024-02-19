d155415bbccb55b309e3029828da36933e8f18c0
charliecaputo <c3caputo@gmail.com>
2024-02-19 14:45:40 -0500

    continuous spin addition

--- code/cont_move.py
+++ code/cont_move.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 # GPIO pins for PUL and DIR
-PUL_PIN = 12
-DIR_PIN = 16
+PUL_PIN = 16
+DIR_PIN = 12
 
 # Set up GPIO mode and pins
 GPIO.setmode(GPIO.BOARD)
@@ -16,19 +16,19 @@ GPIO.output(DIR_PIN, GPIO.HIGH)
 # Function to make one step
 def step():
     GPIO.output(PUL_PIN, GPIO.HIGH)
-    time.sleep(0.005)  # Adjust this delay as needed for your motor
+    time.sleep(0.0001)  # smallest delay possible
     GPIO.output(PUL_PIN, GPIO.LOW)
-    time.sleep(0.005)  # Adjust this delay as needed for your motor
+    time.sleep(0.0001)  # smallest delay possible
 
 # Function to make continuous revolutions
 def continuous_rotation(rev_per_minute):
-    delay = 60.0 / (400 * rev_per_minute)  # Assuming 200 steps per revolution
-    while True:
+    delay = 60.0 / (400 * rev_per_minute)  # 400 steps per revolution
+    for x in range(400): # does one full revolution 
         step()
         time.sleep(delay)
 
 try:
-    continuous_rotation(30)  # Adjust 30 to desired revolutions per minute
+    continuous_rotation(1000)  # Adjust to desired revolutions per minute
 except KeyboardInterrupt:
     pass
 finally:
